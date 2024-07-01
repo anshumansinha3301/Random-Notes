a6e5193d9d681d347eadf362411f93a814bd78c5
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-01 23:05:09 +0530

    Update aptitude.cpp

--- Journals/aptitude.cpp
+++ Journals/aptitude.cpp
@@ -46,4 +46,53 @@ int main() {
 
 
 
-/**/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Data Science Terms
+Dataset - A dataset is a particular instance of data that is ued for analysis or model building at any given time. A dataset comes in different 
+flavors such as numerical data, categorical data, text data, image data, voice data and video data. For beginning data science projects, the most 
+popular type of dataset is a dataset containing numerical data that is typically stored in a comma separated value (CSV) file format.
+
+Data Wrangling - Data wrangling is the process of converting data from its raw form to a tidy form ready for analysis. Data wrangling is an 
+important step in data preprocessing and includes several processes like data importing, data cleaning, data structuring, string processing, HTML
+parsing, handles dates and times, handling missing data and text mining.
+
+Data Visualization - It is one of the main tools used to analyse and study relationships between different variables. Data visualization 
+(e.g., scatter plots, line graphs, bar plots, histograms, qqplots, smooth densities, boxplots, pair plots, heat maps, etc)
+can be used for descriptive analytics. Data visualization is also used in machine learning for data preprocessing and analysis,
+feature selection, model selection, model buliding, model testing, and model evaluation. 
+
+Outliers - an outlier is a data point that is very different from the rest of the dataset. Outliers are very common and are expected
+in large datasets. One common way to detect outliers in a dataset is by using a box plot. Outliers can significantly degrade the predictive power of a 
+machine learning model. Advanced methods for dealing with outliers include the RANSAC method
+
+
+*/
