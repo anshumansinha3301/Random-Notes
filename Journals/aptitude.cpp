0ce2479dda3712db98556a4e0543d7e679f1a98d
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-02 22:27:38 +0530

    Update aptitude.cpp

--- Journals/aptitude.cpp
+++ Journals/aptitude.cpp
@@ -90,7 +90,7 @@ Data Visualization - It is one of the main tools used to analyse and study relat
 (e.g., scatter plots, line graphs, bar plots, histograms, qqplots, smooth densities, boxplots, pair plots, heat maps, etc)
 can be used for descriptive analytics. Data visualization is also used in machine learning for data preprocessing and analysis,
 feature selection, model selection, model buliding, model testing, and model evaluation. 
-  
+   
 Outliers - an outlier is a data point that is very different from the rest of the dataset. Outliers are very common and are expected
 in large datasets. One common way to detect outliers in a dataset is by using a box plot. Outliers can significantly degrade the predictive power of a 
 machine learning model. Advanced methods for dealing with outliers include the RANSAC method
